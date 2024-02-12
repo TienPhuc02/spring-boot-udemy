126109b21a17a43ed58eeacbe82ebd4f606ced92
TienPhuc02 <dtp2352002@gmail.com>
2024-02-12 16:40:13 +0700

    create REST Controller

--- src/main/java/com/example/udemychapter1/Udemychapter1Application.java
+++ src/main/java/com/example/udemychapter1/Udemychapter1Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Udemychapter1Application {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(Udemychapter1Application.class, args);
 	}
 
