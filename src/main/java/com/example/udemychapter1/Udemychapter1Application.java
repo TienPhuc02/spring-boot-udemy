bd40b31a8df335c0cd3a539b11579f3ec1655cc2
TienPhuc02 <dtp2352002@gmail.com>
2024-02-12 16:32:58 +0700

    first commit

---
+++ src/main/java/com/example/udemychapter1/Udemychapter1Application.java
@@ -0,0 +1,13 @@
+package com.example.udemychapter1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Udemychapter1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Udemychapter1Application.class, args);
+	}
+
+}
